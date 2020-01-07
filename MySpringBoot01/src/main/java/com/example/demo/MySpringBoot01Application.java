7271f6c9d34d9d615d584f2b7a323f22e1b2afb1
Administrator <Administrator@9NBKH9D17J767OL>
2020-01-07 16:34:02 +0800

    frist

---
+++ MySpringBoot01/src/main/java/com/example/demo/MySpringBoot01Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBoot01Application {
+
+	public static void main(String[] args) {
+		System.out.println("ssss");
+		SpringApplication.run(MySpringBoot01Application.class, args);
+	}
+
+}
